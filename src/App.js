c7cb21af4e1f9d509d8ceb6520caeb5304c5d2bc
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-11-12 19:15:23 +0600

    google map feature is added for set location

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="mapouter">
+        <div class="gmap_canvas">
+          <iframe
+          title="This is a unique title" 
+            width="100%"
+            height="600px"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=square%20hospital,%20dhanmondi,%20dhaka&t=&z=15&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+    
+        </div>
+      </div>
     </div>
   );
 }
